3d1873ae69998705f18dfa2bb960e2444605fe02
jflyer <2360127010@qq.com>
2024-12-27 13:29:34 +0800

    optime score

--- src/ScoringObject.cpp
+++ src/ScoringObject.cpp
@@ -17,107 +17,65 @@ using namespace DBoW2;
 // epsilon value (this is needed by the KL method)
 const double GeneralScoring::LOG_EPS = log(DBL_EPSILON); // FLT_EPSILON
 
-// ---------------------------------------------------------------------------
-// ---------------------------------------------------------------------------
-
+// Double-pointer traversal reduces the lookup overhead
 double L1Scoring::score(const BowVector &v1, const BowVector &v2) const
 {
-  BowVector::const_iterator v1_it, v2_it;
-  const BowVector::const_iterator v1_end = v1.end();
-  const BowVector::const_iterator v2_end = v2.end();
-  
-  v1_it = v1.begin();
-  v2_it = v2.begin();
-  
+  auto v1_it = v1.begin(), v2_it = v2.begin();
+  const auto v1_end = v1.end(), v2_end = v2.end();
   double score = 0;
-  
+
   while(v1_it != v1_end && v2_it != v2_end)
   {
-    const WordValue& vi = v1_it->second;
-    const WordValue& wi = v2_it->second;
-    
     if(v1_it->first == v2_it->first)
     {
+      const WordValue &vi = v1_it->second;
+      const WordValue &wi = v2_it->second;
       score += fabs(vi - wi) - fabs(vi) - fabs(wi);
-      
-      // move v1 and v2 forward
-      ++v1_it;
+      ++v1_it; 
       ++v2_it;
     }
-    else if(v1_it->first < v2_it->first)
-    {
-      // move v1 forward
-      v1_it = v1.lower_bound(v2_it->first);
-      // v1_it = (first element >= v2_it.id)
-    }
-    else
-    {
-      // move v2 forward
-      v2_it = v2.lower_bound(v1_it->first);
-      // v2_it = (first element >= v1_it.id)
-    }
+    else if(v1_it->first < v2_it->first) 
+      ++v1_it;
+    else 
+      ++v2_it;
   }
-  
-  // ||v - w||_{L1} = 2 + Sum(|v_i - w_i| - |v_i| - |w_i|) 
-  //		for all i | v_i != 0 and w_i != 0 
-  // (Nister, 2006)
-  // scaled_||v - w||_{L1} = 1 - 0.5 * ||v - w||_{L1}
-  score = -score/2.0;
 
-  return score; // [0..1]
+  score = -score / 2.0;
+  return score;
 }
 
-// ---------------------------------------------------------------------------
-// ---------------------------------------------------------------------------
+// ...existing code...
+#include <execution> // C++17 parallel algorithms
 
 double L2Scoring::score(const BowVector &v1, const BowVector &v2) const
 {
-  BowVector::const_iterator v1_it, v2_it;
-  const BowVector::const_iterator v1_end = v1.end();
-  const BowVector::const_iterator v2_end = v2.end();
+  // parallel traversal to speed up scoring
+  std::vector<double> partialScores;
+  partialScores.reserve(std::max(v1.size(), v2.size()));
   
-  v1_it = v1.begin();
-  v2_it = v2.begin();
-  
-  double score = 0;
-  
-  while(v1_it != v1_end && v2_it != v2_end)
-  {
-    const WordValue& vi = v1_it->second;
-    const WordValue& wi = v2_it->second;
-    
-    if(v1_it->first == v2_it->first)
+  std::for_each(std::execution::par, v1.begin(), v1.end(), [&](const auto &p1) {
+    auto it = v2.find(p1.first);
+    if(it != v2.end())
     {
-      score += vi * wi;
-      
-      // move v1 and v2 forward
-      ++v1_it;
-      ++v2_it;
+      // skips pure zero-valued elements
+      if(p1.second != 0.0 || it->second != 0.0)
+      {
+        partialScores.push_back(std::fabs(p1.second - it->second)
+                                - std::fabs(p1.second)
+                                - std::fabs(it->second));
+      }
     }
-    else if(v1_it->first < v2_it->first)
-    {
-      // move v1 forward
-      v1_it = v1.lower_bound(v2_it->first);
-      // v1_it = (first element >= v2_it.id)
-    }
-    else
-    {
-      // move v2 forward
-      v2_it = v2.lower_bound(v1_it->first);
-      // v2_it = (first element >= v1_it.id)
-    }
-  }
-  
-  // ||v - w||_{L2} = sqrt( 2 - 2 * Sum(v_i * w_i) )
-	//		for all i | v_i != 0 and w_i != 0 )
-	// (Nister, 2006)
-	if(score >= 1) // rounding errors
-	  score = 1.0;
-	else
-    score = 1.0 - sqrt(1.0 - score); // [0..1]
+  });
 
-  return score;
+  double score = 0;
+  for(const auto &val : partialScores)
+  {
+    score += val;
+  }
+
+  return -score / 2.0;
 }
+
 
 // ---------------------------------------------------------------------------
 // ---------------------------------------------------------------------------
